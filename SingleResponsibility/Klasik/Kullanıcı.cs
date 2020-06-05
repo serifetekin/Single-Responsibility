6cd656bb451b9c1e046f4552e14e9c5de10ae69f
Şerife Tekin <serifetekin2451@gmail.com>
2020-06-05 16:00:24 +0300

    Solid prensiblerinin ilki

---
+++ SingleResponsibility/Klasik/Kullanıcı.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility.Klasik
+{
+    class Kullanıcı
+    {
+        public int Id;
+        public string Adi;
+        public string Soyadi;
+        string KullaniciAdi;
+        string Parola;
+
+        public void Ekle(string adi, string soyadi)
+        {
+            KullaniciAdi = adi;
+            Soyadi = soyadi;
+            Console.WriteLine(adi + " " + soyadi + " kaydı eklendi.");
+        }
+
+        public void Guncelle(int id, string adi, string soyadi)
+        {
+            KullaniciAdi = adi;
+            Soyadi = soyadi;
+            Console.WriteLine(id + " refnolu kayıt " + adi + " " + soyadi + " olarak güncellendi");
+        }
+
+         public void Sil(int id)
+        {
+            Console.WriteLine(id + " refnolu kullanıcı silindi");
+        }
+
+         public Kullanıcı Getir(int id)
+        {
+            Kullanıcı k = new Kullanıcı() { Id = id, KullaniciAdi = "Şerife", Parola = "Tekin", Adi = "Şerife", Soyadi = "Tekin" };
+            Console.WriteLine(k.KullaniciAdi + " " + k.Parola + "kullanıcı bulundu");
+            return k;
+        }
+
+        public bool ChechUser(string kullaniciadi, string parola)
+        {
+            KullaniciAdi = kullaniciadi;
+            Parola = parola;
+
+            Console.WriteLine(KullaniciAdi + " " + Parola + "ile giriş yapıldı");
+            return true;
+        }
+
+        public void Logout(int id)
+        {
+            Console.WriteLine(id + " refnolu kullanıcı sstemden çıktı");
+        }
+    }
+}
---
+++ SingleResponsibility/Kullanıcı.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    partial class Kullanıcı // gelişime kapalı geliştirmek için yeni bir class yazmak gerekir.
+    {
+        public int Id;
+        public string Adi;
+        public string Soyadi;
+        public string KullaniciAdi;
+        public string Parola;
+
+    }
+    class CRUD
+    {
+        public void Ekle(Kullanıcı k)
+        {
+            Console.WriteLine(k.Adi + " " + k.Soyadi + " kaydı eklendi.");
+        }
+
+        public void Guncelle(Kullanıcı k)
+        {
+            Console.WriteLine(k.Id + " refnolu kayıt " + k.Adi + " " + k.Soyadi + " olarak güncellendi");
+        }
+
+        public void Sil(Kullanıcı k)
+        {
+            Console.WriteLine(k.Id + " refnolu kullanıcı silindi");
+        }
+
+        public Kullanıcı Getir(int id)
+        {
+            Kullanıcı k = new Kullanıcı() { Id = id, KullaniciAdi = "Şerife", Parola = "Tekin", Adi = "Şerife", Soyadi = "Tekin" };
+            Console.WriteLine(id+ "refnolu kayıt bulundu.");
+            return k;
+        }
+    }
+
+    class KullanıcıAuthentication
+    {
+        public bool ChechUser(string kullaniciadi, string parola)
+        {
+            Console.WriteLine(kullaniciadi + " " + parola + "ile giriş yapıldı");
+            return true;
+        }
+
+        public void Logout(int id)
+        {
+            Console.WriteLine(id + " refnolu kullanıcı sstemden çıktı");
+        }
+    }
+}
